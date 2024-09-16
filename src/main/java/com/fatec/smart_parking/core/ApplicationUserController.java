de053deeda3a649321fe1a7c85a426c3b98ef40e
VictorVFM <victorhugoferreira452@gmail.com>
2024-09-15 21:39:46 -0300

    wip: start implementation to send email

--- src/main/java/com/fatec/smart_parking/core/ApplicationUserController.java
+++ src/main/java/com/fatec/smart_parking/core/ApplicationUserController.java
@@ -56,4 +56,8 @@ public class ApplicationUserController{
         return ResponseEntity.ok().build();
     }
 
+    @PostMapping("/reset-password")
+    public ResponseEntity resetPassword(@RequestBody @Valid String email){
+
+    }
 }
---
+++ src/main/java/com/fatec/smart_parking/core/email/EmailService.java
@@ -0,0 +1,10 @@
+package com.fatec.smart_parking.core.email;
+
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService extends SimpleMailMessage {
+
+}
