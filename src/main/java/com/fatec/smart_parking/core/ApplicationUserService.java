c62797eaf05020df076db5b2852a1c65286257b1
VictorVFM <victorhugoferreira452@gmail.com>
2024-09-18 18:41:32 -0300

    feat: add new password generator

--- src/main/java/com/fatec/smart_parking/core/ApplicationUserService.java
+++ src/main/java/com/fatec/smart_parking/core/ApplicationUserService.java
@@ -1,5 +1,6 @@
 package com.fatec.smart_parking.core;
 
+import java.security.SecureRandom;
 import java.util.Optional;
 
 import com.fatec.smart_parking.core.email.EmailService;
@@ -8,6 +9,7 @@ import com.fatec.smart_parking.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,6 +20,21 @@ public class ApplicationUserService implements UserDetailsService{
 
     @Autowired
     private EmailService emailService;
+
+
+
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final int PASSWORD_LENGTH = 12;
+    private final SecureRandom secureRandom = new SecureRandom();
+
+    public String generateRandomPassword() {
+        StringBuilder password = new StringBuilder(PASSWORD_LENGTH);
+        for (int i = 0; i < PASSWORD_LENGTH; i++) {
+            int randomIndex = secureRandom.nextInt(CHARACTERS.length());
+            password.append(CHARACTERS.charAt(randomIndex));
+        }
+        return password.toString();
+    }
 
     @Override
     public User loadUserByUsername(String email) {
@@ -30,9 +47,83 @@ public class ApplicationUserService implements UserDetailsService{
 
 
     public void resetPassword(String email){
-        this.emailService.sendSimpleMessage(email,"Reset Password","Your new password has been reset");
-    }
+        Optional<User> optionalUser = userRepository.findByEmail(email);
+        String newPassword = generateRandomPassword();
+        String encryptedPassword = new BCryptPasswordEncoder().encode(newPassword);
+        if(optionalUser.isPresent()){
+            optionalUser.get().setPassword(encryptedPassword);
+            this.userRepository.save(optionalUser.get());
+            this.emailService.sendHtmlMessage(email,"Sua senha foi redefinida",
+                    "<!DOCTYPE html>\n" +
+                            "<html lang=\"pt-BR\">\n" +
+                            "<head>\n" +
+                            "    <meta charset=\"UTF-8\">\n" +
+                            "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+                            "    <title>Senha Gerada</title>\n" +
+                            "    <style>\n" +
+                            "        body {\n" +
+                            "            font-family: Arial, sans-serif;\n" +
+                            "            background-color: #f4f4f4;\n" +
+                            "            padding: 20px;\n" +
+                            "        }\n" +
+                            "        .container {\n" +
+                            "            background-color: #ffffff;\n" +
+                            "            padding: 20px;\n" +
+                            "            border-radius: 5px;\n" +
+                            "            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);\n" +
+                            "            max-width: 600px;\n" +
+                            "            margin: auto;\n" +
+                            "        }\n" +
+                            "        h1 {\n" +
+                            "            color: #333;\n" +
+                            "        }\n" +
+                            "        p {\n" +
+                            "            font-size: 16px;\n" +
+                            "            line-height: 1.5;\n" +
+                            "            color: #666;\n" +
+                            "        }\n" +
+                            "        .password {\n" +
+                            "            font-weight: bold;\n" +
+                            "            font-size: 18px;\n" +
+                            "            color: #007bff;\n" +
+                            "        }\n" +
+                            "        .button {\n" +
+                            "            display: inline-block;\n" +
+                            "            padding: 10px 20px;\n" +
+                            "            margin-top: 20px;\n" +
+                            "            background-color: #007bff;\n" +
+                            "            color: #ffffff;\n" +
+                            "            text-decoration: none;\n" +
+                            "            border-radius: 5px;\n" +
+                            "        }\n" +
+                            "        .footer {\n" +
+                            "            margin-top: 20px;\n" +
+                            "            font-size: 12px;\n" +
+                            "            color: #999;\n" +
+                            "            text-align: center;\n" +
+                            "        }\n" +
+                            "    </style>\n" +
+                            "</head>\n" +
+                            "<body>\n" +
+                            "    <div class=\"container\">\n" +
+                            "        <h1>Senha Gerada</h1>\n" +
+                            "        <p>Olá,</p>\n" +
+                            "        <p>Uma nova senha foi gerada para você:</p>\n" +
+                            "        <p class=\"password\">"+newPassword+"</p>\n" +
+                            "        <p>Use esta senha para acessar sua conta. Você pode alterá-la a qualquer momento nas configurações da sua conta.</p>\n" +
+                            "        <p>Se precisar de ajuda, não hesite em entrar em contato.</p>\n" +
+                            "        <p>Atenciosamente,<br>A equipe do Smart Parking</p>\n" +
+                            "    </div>\n" +
+                            "    <div class=\"footer\">\n" +
+                            "        <p>© 2024 Smart Parking. Todos os direitos reservados.</p>\n" +
+                            "    </div>\n" +
+                            "</body>\n" +
+                            "</html>\n");
+        }
 
+
+
+    }
 
 
 }
--- src/main/java/com/fatec/smart_parking/core/email/EmailService.java
+++ src/main/java/com/fatec/smart_parking/core/email/EmailService.java
@@ -1,10 +1,17 @@
 package com.fatec.smart_parking.core.email;
 
+import jakarta.mail.internet.InternetAddress;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+
+import java.io.UnsupportedEncodingException;
 
 @Service
 public class EmailService {
@@ -15,14 +22,20 @@ public class EmailService {
     @Value("${spring.mail.username}")
     private String sender;
 
-    public void sendSimpleMessage(String to, String subject, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(sender);
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
-        emailSender.send(message);
+
+    public void sendHtmlMessage(String to, String subject, String htmlContent) {
+        MimeMessage message = emailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(new InternetAddress(sender,"Smart Park Support","UTF-8"));
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(htmlContent, true);
+            emailSender.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
     }
-
-
 }
